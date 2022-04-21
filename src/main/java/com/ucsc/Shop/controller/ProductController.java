c8c13450114b70a0a97db617496404bd1e3d5ee3
chathura <chathura.maadura11@gmail.com>
2022-04-22 03:04:02 +0530

    test commit

--- src/main/java/com/ucsc/Shop/controller/ProductController.java
+++ src/main/java/com/ucsc/Shop/controller/ProductController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
+//map with react project
 @CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
     @Autowired
